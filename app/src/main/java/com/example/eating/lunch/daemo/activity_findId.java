900e5d19bb5bc4a30d86da4d9b585407ea3c6ead
fj2008 <fj2008@naver.com>
2021-06-05 19:05:49 +0900

    test

--- app/src/main/java/com/example/eating/lunch/daemo/activity_findId.java
+++ app/src/main/java/com/example/eating/lunch/daemo/activity_findId.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class activity_findId extends AppCompatActivity {
-
+    //asldkfjas;ldkfjas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
