bb211bbea32dbaa4710547ad6b0cb777b6c0ca49
fj2008 <fj2008@naver.com>
2021-06-05 19:59:31 +0900

    commit

---
+++ app/src/main/java/com/example/eating/lunch/daemo/activity_food.java
@@ -0,0 +1,14 @@
+package com.example.eating.lunch.daemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food);
+    }
+}
