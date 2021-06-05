9769779b40e547967b03e68fb91eb2c8433f08a2
DESKTOP-23AK76E\USER-PC <heansum1556@gmail.com>
2021-06-05 21:36:09 +0900

    server branch update1

--- app/src/main/java/com/example/eating/lunch/daemo/activity_food.java
+++ app/src/main/java/com/example/eating/lunch/daemo/activity_food.java
@@ -3,12 +3,18 @@ package com.example.eating.lunch.daemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_food extends AppCompatActivity {
+    TextView foodView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        TextView foodView = findViewById(R.id.textViewFood);
+
+
     }
 }
--- app/src/main/java/com/example/eating/lunch/daemo/activity_join.java
+++ app/src/main/java/com/example/eating/lunch/daemo/activity_join.java
@@ -3,6 +3,8 @@ package com.example.eating.lunch.daemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_join extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class activity_join extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        // ID 체크 버튼
+        Button btnCheckId = findViewById(R.id.CheckId);
+        // 회원가입 버튼
+        Button btnJoinInServer = findViewById(R.id.Join);
+        
+        // ID 체크 버튼 기능
+        btnCheckId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // DB에 있는 아이디를 체크
+            }
+        });
+
+        btnJoinInServer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // 비밀번호 동일 체크 되어있는지 확인
+
+                // 비밀번호 ok 이메일 확인 ok
+
+                // 완벽하다면 통과되어 DB에 저장
+
+            }
+        });
+
     }
 }
